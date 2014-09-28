8957f4d2e9fb2e2860664bd9b3a49d20a42ee52a
Ivorius <lukastenbrink@googlemail.com>
2014-09-28 14:52:39 +0200

    Give plank saw a fitting bounding box

--- src/main/java/ivorius/yegamolchattels/blocks/BlockPlankSaw.java
+++ src/main/java/ivorius/yegamolchattels/blocks/BlockPlankSaw.java
@@ -1,14 +1,18 @@
 package ivorius.yegamolchattels.blocks;
 
 import ivorius.ivtoolkit.blocks.IvBlockMultiblock;
+import ivorius.ivtoolkit.blocks.IvMultiBlockHelper;
 import ivorius.ivtoolkit.blocks.IvTileEntityMultiBlock;
 import ivorius.ivtoolkit.network.IvNetworkHelperServer;
+import ivorius.yegamolchattels.YGCMultiBlockHelper;
 import ivorius.yegamolchattels.YeGamolChattels;
 import ivorius.yegamolchattels.items.ItemSaw;
 import ivorius.yegamolchattels.items.YGCItems;
 import net.minecraft.block.material.Material;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 /**
@@ -70,5 +74,21 @@ public class BlockPlankSaw extends IvBlockMultiblock
     public TileEntity createNewTileEntity(World var1, int var2)
     {
         return new TileEntityPlankSaw();
+    }
+
+    @Override
+    public void setBlockBoundsBasedOnState(IBlockAccess blockAccess, int x, int y, int z)
+    {
+        IvTileEntityMultiBlock tileEntity = getValidatedTotalParent(this, blockAccess, x, y, z);
+
+        if (tileEntity instanceof TileEntityPlankSaw)
+            setBlockBounds(YGCMultiBlockHelper.boundsIntersection(tileEntity.getRotatedBB(-1, -1, -0.45, 2, 1.8, 1.45), x, y, z));
+        else
+            setBlockBounds(0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f);
+    }
+
+    public void setBlockBounds(AxisAlignedBB bb)
+    {
+        setBlockBounds((float) bb.minX, (float) bb.minY, (float) bb.minZ, (float) bb.maxX, (float) bb.maxY, (float) bb.maxZ);
     }
 }
