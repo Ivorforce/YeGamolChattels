edc8e75fb5dd7c8992b12f3798b87a23a56ff56b
Ivorius <lukastenbrink@googlemail.com>
2014-09-30 16:24:06 +0200

    Bump version to 1.1

--- src/main/java/ivorius/yegamolchattels/YeGamolChattels.java
+++ src/main/java/ivorius/yegamolchattels/YeGamolChattels.java
@@ -48,7 +48,7 @@ import java.util.Collections;
 public class YeGamolChattels
 {
     public static final String MODID = "yegamolchattels";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1";
     public static final String NAME = "Ye Gamol Chattels";
 
     @Instance(value = MODID)
